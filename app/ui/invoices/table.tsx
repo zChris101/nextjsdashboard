53e5f162a9ea2ccda05cb534a81aea357ccbc10a
zChris101 <chrstnmusico@gmail.com>
2023-12-01 22:46:48 +0800

    stable na tungod sa alt

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,6 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
+                        alt='image ragud'
                       />
                       <p>{invoice.name}</p>
                     </div>
