f90a93723097b98a18ed6fadb72e2d49304f8f4f
zChris101 <chrstnmusico@gmail.com>
2023-12-11 21:14:18 +0800

    added vercel.json

--- .next/static/chunks/app/dashboard/invoices/[id]/edit/not-found-10f2fcd6ea2b61c4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[318],{9648:function(n,e,u){Promise.resolve().then(u.t.bind(u,8326,23))}},function(n){n.O(0,[326,971,472,744],function(){return n(n.s=9648)}),_N_E=n.O()}]);
